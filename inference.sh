9c0e4bb0cd8c58659be3cc1b107844e98e3d95a7
limhyoseok <nonamedoud@gmail.com>
2021-11-30 16:33:45 +0000

    feat) 대회용 inferenence 명령어

--- inference.sh
+++ inference.sh
@@ -1,1 +1,1 @@
-python inference.py --model_dir /opt/ml/code/exp/latest2 --weight best.pt --img_root /opt/ml/data/test/
+python inference.py --model_dir /opt/ml/code/exp/latest --weight best.pt --img_root /opt/ml/data/test/
