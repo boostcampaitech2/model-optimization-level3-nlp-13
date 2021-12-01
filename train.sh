6a7f53d1bb218e1c0dd2458e8addcd9482870084
limhyoseok <nonamedoud@gmail.com>
2021-12-01 09:02:29 +0000

    feat) train.sh 형식 변경

--- train.sh
+++ train.sh
@@ -1,1 +1,5 @@
-python train.py --model configs/model/mobilenetv3.yaml --data configs/data/taco.yaml --eval "/opt/ml/code/exp/latest/best.pt" --savefolder_name 'eval_test'
+python train.py --model configs/model/mobilenetv3.yaml \
+                --data configs/data/taco.yaml \
+                --savefolder_name 'mobile_v3_small_256'\
+                --train
+                # --eval "/opt/ml/code/exp/latest/best.pt"\
