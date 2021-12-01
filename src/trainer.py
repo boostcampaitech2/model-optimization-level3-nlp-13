4667fa112eed9e98895cd5c1a3906ab2fd77ff53
limhyoseok <nonamedoud@gmail.com>
2021-12-01 13:59:59 +0000

    feat)train for model compare

--- src/trainer.py
+++ src/trainer.py
@@ -113,128 +113,8 @@ class TorchTrainer:
         self.verbose = verbose
         self.device = device
         self.save_folder = save_folder
-        wandb.init(project="model_opti")
+        wandb.init(project="model_compare")
         wandb.run.name = name
-
-  
-    def train_one_epoch(
-        self,
-        train_dataloader: DataLoader,
-        label_list,
-        n_epoch: int = 1,
-    ) -> Tuple[float, float]:
-        
-        # """Train model.
-
-        # Args:
-        #     train_dataloader: data loader module which is a iterator that returns (data, labels)
-        #     n_epoch: number of total epochs for training
-        #     val_dataloader: dataloader for validation
-
-        # Returns:
-        #     loss and accuracy
-        # """
-        
-        # best_test_acc = -1.0
-        # best_test_f1 = -1.0
-        
-        for epoch in range(n_epoch):
-            running_loss, correct, total = 0.0, 0, 0
-            preds, gt = [], []
-            pbar = tqdm(enumerate(train_dataloader), total=len(train_dataloader))
-            self.model.train()
-            for batch, (data, labels) in pbar:
-                data, labels = data.to(self.device), labels.to(self.device)
-
-                if self.scaler:
-                    with torch.cuda.amp.autocast():
-                        outputs = self.model(data)
-                else:
-                    outputs = self.model(data)
-                outputs = torch.squeeze(outputs)
-                loss = self.criterion(outputs, labels)
-
-                self.optimizer.zero_grad()
-
-                if self.scaler:
-                    self.scaler.scale(loss).backward()
-                    self.scaler.step(self.optimizer)
-                    self.scaler.update()
-                else:
-                    loss.backward()
-                    self.optimizer.step()
-
-                _, pred = torch.max(outputs, 1)
-                total += labels.size(0)
-                correct += (pred == labels).sum().item()
-                preds += pred.to("cpu").tolist()
-                gt += labels.to("cpu").tolist()
-
-                running_loss += loss.item()
-
-                train_acc = (correct / total) * 100
-                train_f1 = f1_score(y_true=gt, y_pred=preds, labels=label_list, average='macro', zero_division=0)
-                pbar.update()
-                pbar.set_description(
-                    f"Train: [{epoch + 1:03d}] "
-                    f"Loss: {(running_loss / (batch + 1)):.3f}, "
-                    f"Acc: {train_acc:.2f}% "
-                    f"F1(macro): {train_f1:.2f}"
-                )
-            pbar.close()
-
-        return train_acc, train_f1
-    
-
-    def eval(
-        self, model: nn.Module, test_dataloader: DataLoader, device
-    ) -> Tuple[float, float, float]:
-    
-        n_batch = _get_n_batch_from_dataloader(test_dataloader)
-
-        running_loss = 0.0
-        preds = []
-        gt = []
-        correct = 0
-        total = 0
-
-        num_classes = _get_len_label_from_dataset(test_dataloader.dataset)
-        label_list = [i for i in range(num_classes)]
-
-        pbar = tqdm(enumerate(test_dataloader), total=len(test_dataloader))
-        model.to(device)
-        model.eval()
-        for batch, (data, labels) in pbar:
-            data, labels = data.to(device), labels.to(device)
-
-            if self.scaler:
-                with torch.cuda.amp.autocast():
-                    outputs = model(data)
-            else:
-                outputs = model(data)
-            outputs = torch.squeeze(outputs)
-            running_loss += self.criterion(outputs, labels).item()
-
-            _, pred = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (pred == labels).sum().item()
-
-            preds += pred.to("cpu").tolist()
-            gt += labels.to("cpu").tolist()
-            pbar.update()
-            pbar.set_description(
-                f" Val: {'':5} Loss: {(running_loss / (batch + 1)):.3f}, "
-                f"Acc: {(correct / total) * 100:.2f}% "
-                f"F1(macro): {f1_score(y_true=gt, y_pred=preds, labels=label_list, average='macro', zero_division=0):.2f}"
-            )
-        loss = running_loss / len(test_dataloader)
-        accuracy = correct / total
-        f1 = f1_score(
-            y_true=gt, y_pred=preds, labels=label_list, average="macro", zero_division=0
-        )
-        return loss, f1, accuracy
-
-
 
     def train(
         self,
@@ -315,18 +195,18 @@ class TorchTrainer:
                     "epoch" : epoch
                 })
 
-            if best_test_f1 > test_f1:
-                continue
-            best_test_acc = test_acc
-            best_test_f1 = test_f1
-            print(f"Model saved. Current best test f1: {best_test_f1:.3f}")
-            save_model(
-                model=self.model,
-                path=self.model_path,
-                data=data,
-                device=self.device,
-            )
-            torch.save(self.model, os.path.join(self.save_folder, "model.pt"))
+            # if best_test_f1 > test_f1:
+            #     continue
+            # best_test_acc = test_acc
+            # best_test_f1 = test_f1
+            # print(f"Model saved. Current best test f1: {best_test_f1:.3f}")
+            # save_model(
+            #     model=self.model,
+            #     path=self.model_path,
+            #     data=data,
+            #     device=self.device,
+            # )
+            # torch.save(self.model, os.path.join(self.save_folder, "model.pt"))
 
         return best_test_acc, best_test_f1
 
